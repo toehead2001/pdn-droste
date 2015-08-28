590893ffbfbd10afd0581c4c606d47f66b57fdc1
toehead2001 <toe_head2001@yahoo.com>
2015-08-28 16:34:52 -0600

    Prepare for release of 1.0

--- droste/Properties/AssemblyInfo.cs
+++ droste/Properties/AssemblyInfo.cs
@@ -1,33 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Droste")]
-[assembly: AssemblyDescription("Droste plugin for Paint.NET. Create Escher-like pictures")]
+[assembly: AssemblyDescription("Create Escher-like pictures")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Droste")]
-[assembly: AssemblyCopyright("Copyright 2008, Pieter-Jan Busschaert")]
+[assembly: AssemblyCopyright("Copyright 2008, Pieter-Jan Busschaert, toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("78783A44-CC4A-11DC-AD57-3FD755D89593")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
